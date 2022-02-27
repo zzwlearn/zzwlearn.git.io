b25797204cc9e117cb30cfbea7a78f451ed3bdd0
zzwlearn <86993551+zzwlearn@users.noreply.github.com>
2022-02-27 20:44:35 +0800

    Add files via upload

---
+++ thumbnail.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",function(event){
+        let targetImage = document.querySelector("#smart-image");
+        targetImage.addEventListener("click",function(){
+            targetImage.classList.remove("small");
+        })
+})
