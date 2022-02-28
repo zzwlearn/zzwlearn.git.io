a12e1a1d350446c870a165f89ffb0f3f75e49c5e
zzwlearn <86993551+zzwlearn@users.noreply.github.com>
2022-02-28 15:28:18 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded",function(event){
         let targetImage = document.querySelector("#smart-image");
-        targetImage.addEventListener("click",function(){
-                
+        targetImage.addEventListener("click",function(){ 
             targetImage.classList.remove("small");
         })
-})
+        
+        }
+
+)
